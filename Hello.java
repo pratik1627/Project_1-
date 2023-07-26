364558db5768fc8c2a7ebd97f70e5659782697cb
pratik1627 <140620525+pratik1627@users.noreply.github.com>
2023-07-26 21:18:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,207 @@
+/*import java.util.Scanner;
+
+public class Hello
+{
+	public static void main(String[] args)
+   {
+	    Scanner sc = new Scanner(System.in);
+		int size;
+		//double d;
+               // String s;
+                String ss;
+		System.out.println("Enter size of your array : ");
+		size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		
+		for(int i=0;i<size;i++)
+                 {
+		    //System.out.println("Enter element "+(i+1)+" : ");
+		    arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Reversed array is :");
+		
+		for(int i=size-1;i>=0;i--)
+                {
+		    System.out.print(arr[i]+" ");
+		}
+}
+}*/
+/*
+import java.util.Scanner;
+public class Hello{
+public static void main(String[]args)
+{
+    Scanner sc = new Scanner(System.in);
+  //System.out.println("enter the size of the array : ");
+
+   String ss = sc.nextLine();
+   int size  =sc.nextInt();
+  
+    double dd = sc.nextDouble();
+
+System.out.println("My number  is : "+ ss);
+System.out.println("My name is : "+ size);
+System.out.println("My Shoose size  is : "+ dd);
+
+}}
+
+
+*/
+/*
+import java.util.*;
+class Hello
+{
+public static void main(String[]args)
+{  //ArrayList<Integer> arr = new ArrayList<Integer>();
+  //List <Integer>arr= Arrays.asList(10,20,30,40,50);
+
+//arr.add(10);
+//arr.add(30);
+//arr.add(40);
+//arr.add(50);
+//arr.add(60);
+//arr.add(70);
+
+Iterator it = arr.iterator();
+
+while(it.hasNext())
+{
+   System.out.println(it.next());
+
+}
+}
+}*/
+
+/*
+class Hello
+{
+
+ static int smallest(int x,int y ,int z)
+{   
+int c=0;
+   while(x!= 0 && y!=0 && z!=0)
+{
+ x--;
+ y--;
+ z--;
+ c++;
+
+}
+return c;
+
+
+}
+public static void main(String[]args)
+{
+   int x=10,y=20,z=30;
+   
+        System.out.printf("Minimum of 3"
+                              + " numbers is %d",
+                          smallest(x, y, z));
+}
+
+}*/
+import java.util.Scanner;
+class Hello{
+public static void main(String[]args)
+{     
+   Scanner sc= new Scanner(System.in);
+    int size=sc.nextInt();
+   System.out.println("Enter the array element size");
+   int arr[] = new  int[size];
+  for(in i=0;i<=size;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
+System.out.println("Array Elements are : " arr[i]);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
